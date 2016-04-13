0ce56c3c1996596c3443628666e1065387ce0b4e
jenno <jenno.cabrito@gmail.com>
2016-04-13 11:20:08 +0400

    Initial Commit : Practise C# Strategy Base Design pattern

---
+++ AMSCore/trunk/AMSCore/Base/GenericStorage.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+using tarsierSQL = TarsierEyes.MySQL;
+using tarsierSQLString = TarsierEyes.Common.SQLStrings;
+using System.Diagnostics;
+
+
+/**
+ *  AMSCore Generic Class storage
+ *  @author Jcabrito <Oct. 27, 2015>
+ *  @return mixed
+ */
+namespace AMSCore
+{
+    public class GenericStorage
+    {
+
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private string _constring    = string.Empty;
+
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private string _apiEndpoint  = string.Empty;        
+
+        public string connectionString
+        {
+            get
+            {
+                return _constring;
+            }
+            set
+            {
+                _constring = value;
+            }
+        }
+
+        public string APIEndpoint
+        {
+            get
+            {
+                return _apiEndpoint;
+            }
+            set
+            {
+                _apiEndpoint = value;
+            }
+        }
+
+        public string httpMethod { get; set; }
+
+        public DataTable getTable(string sql)
+        {
+            DataTable table = new DataTable();
+
+            try
+            {
+
+                using (var con = new MySqlConnection(this.connectionString))
+                using (var adapter = new MySqlDataAdapter(sql, con))
+                using (new MySqlCommandBuilder(adapter))
+                {
+                    adapter.Fill(table);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+            return ((table.Rows.Count <= 0) ? null : table);
+            
+        }
+
+        /**
+         * temporary set to non blocking code.
+         * always return boolean true
+         */
+        public bool queueFailedRequest(string module, string reference_id)
+        {
+
+            bool response = true;
+
+            tarsierSQL.Que processSql = null;
+
+            if (string.IsNullOrEmpty(module) || string.IsNullOrEmpty(reference_id))
+                return response;
+
+            string sql = "call usp_tmpUpdates('" + module + "','" + reference_id + "')";
+            
+            processSql = tarsierSQL.Que.Execute(this.connectionString.ToString(),
+                    sql, tarsierSQL.Que.ExecutionEnum.ExecuteNonQuery);
+
+            return response;
+
+        }
+
+        /**
+         * temporary set to non blocking code.
+         * always return boolean true
+         */
+        public bool executeQuery(string sql)
+        {
+
+            bool response = true;
+
+            tarsierSQL.Que processSql = null;
+
+            if (string.IsNullOrEmpty(sql))
+                return response;
+
+            processSql = tarsierSQL.Que.Execute(this.connectionString.ToString(),
+                    sql, tarsierSQL.Que.ExecutionEnum.ExecuteNonQuery);
+
+            return response;
+
+        }
+
+        public object getQueryvalue(string sql)
+        {
+
+            object processSql = null;
+
+            if (string.IsNullOrEmpty(sql))
+                return processSql;
+
+            processSql = tarsierSQL.Que.GetValue(this.connectionString.ToString(), sql);
+
+            return processSql;
+
+        }
+
+    }
+}
---
+++ AMSCore/trunk/AMSCore/Datasets/Users.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AMSCore
+{
+    public class Users
+    {
+        public long user_id { get; set; }
+
+        public string username { get; set; }
+
+        public string password { get; set; }
+
+        public string name { get; set; }
+
+        public string position { get; set; }
+
+        public int usertype { get; set; }
+
+        public int rfqApprover { get; set; }
+
+        public int rfqAmount { get; set; }
+
+        public string email { get; set; }
+
+        public string Status { get; set; }
+
+        public virtual List<Location> UserSites { get; set; }
+    }
+
+    public class Location
+    {
+        public long SiteID { get; set; }
+
+        public string TMPLocation { get; set; }
+
+        public string SiteCode { get; set; }
+
+        public string SiteName { get; set; }
+
+        public byte stat { get; set; }
+
+        public bool Selected { get; set; }
+    }
+}
